414cd2f956ccab7a97de28ed5b0841ea8a7fbf94
jagan12345678 <jagankumar0813@gmail.com>
2024-12-11 20:02:17 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <p className="text-blue-950 font-extrabold m-5">Let create the new react Netflix project </p>
+      
+    </div>
+  );
+}
+
+export default App;
