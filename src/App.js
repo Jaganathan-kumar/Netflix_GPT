dd95f7682de4c1b23ff193380c11c219d2f42110
jagan12345678 <jagankumar0813@gmail.com>
2024-12-14 21:33:46 +0530

    setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 
+import Body from "./components/Body"
 
 function App() {
   return (
-    <div className="App">
-      <p className="text-blue-950 font-extrabold m-5">Let create the new react Netflix project </p>
-      
-    </div>
+   <Body />
+
   );
 }
 
